c1cfb9e9ba015cbf2d9ae161750a938672d7258b
Greghiski <vg.hernandez@hotmail.com>
2022-12-12 18:18:16 -0300

    Initial commit

---
+++ precache-manifest.23088eee25428555d7245f6b33a51144.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c1a52ed4084a7891f09",
+    "url": "./static/css/main.4023e0a6.chunk.css"
+  },
+  {
+    "revision": "4c1a52ed4084a7891f09",
+    "url": "./static/js/main.b6fbdd24.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "28221d67889e07768839",
+    "url": "./static/css/2.1c8b57b5.chunk.css"
+  },
+  {
+    "revision": "28221d67889e07768839",
+    "url": "./static/js/2.4859621a.chunk.js"
+  },
+  {
+    "revision": "8449c7a91d162b2c8491ec472372bb66",
+    "url": "./static/media/icon12.8449c7a9.png"
+  },
+  {
+    "revision": "58d9364e5ca108df7b56adbb8b46458d",
+    "url": "./static/media/2.58d9364e.png"
+  },
+  {
+    "revision": "ff175a4251c4efbf14cc83a06019befe",
+    "url": "./static/media/capa3.ff175a42.jpeg"
+  },
+  {
+    "revision": "793a15297f188b6868720f5c4888096c",
+    "url": "./static/media/4.793a1529.svg"
+  },
+  {
+    "revision": "dfb7bab18185936b3a964a556c1c258f",
+    "url": "./index.html"
+  }
+];
